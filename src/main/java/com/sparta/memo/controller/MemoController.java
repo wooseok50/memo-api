faeb861031b625bf80347c5c9587e44958ecd32f
wooseok <wooseok6008@gmail.com>
2024-01-18 19:34:21 +0900

    강한 결합을 느슨한 결합으로

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -11,11 +11,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MemoController {
-
     private final MemoService memoService;
 
-    public MemoController(JdbcTemplate jdbcTemplate) {
-        this.memoService = new MemoService(jdbcTemplate);
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
     @PostMapping("/memos")
