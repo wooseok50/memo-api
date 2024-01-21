3ef2ce301edcb5dc2d8635c5de4785ffce849e8f
wooseok <wooseok6008@gmail.com>
2024-01-22 01:08:31 +0900

    Query Methods

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -2,9 +2,11 @@ package com.sparta.memo.repository;
 
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 // @Repository 자동으로 설정됨.
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    // List<Memo> findAllByUsername(String username);
 }
